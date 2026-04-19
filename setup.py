21ed3ea0f895aeb391e70c3034f17fdca659d3e1
saurav3k2 <saurav3k2@gmail.com>
2026-04-19 18:17:19 +0530

    dvc initialized

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Builld ml project that predicts food delivery time in minutes.',
+    author='saurav',
+    license='',
+)
